c9dbb413ba8ae7558a63b837e10e1479c787d1b9
zzundi <dlwnsgur0803@naver.com>
2022-05-03 23:25:06 +0900

    algo

--- algorithm/이모티콘.py
+++ algorithm/이모티콘.py
@@ -1,12 +1,12 @@
 n = int(input())
 dp = [0]*(n+1)
-
 for i in range(2, n+1):
     noc = []
     for j in range(1, i):
+        f = dp[j] + (i-j)//j
         if (i-j)%j == 0:
-            noc.append(dp[j] + (i-j)//j + 1)
+            noc.append(f + 1)
         else:
-            noc.append(dp[j] + (i-j)//j + 2 + j*((i-j)//j+1)+j-i)
+            noc.append(f + 2 + j*((i-j)//j+2)-i)
     dp[i] = min(noc)
 print(dp[n])
